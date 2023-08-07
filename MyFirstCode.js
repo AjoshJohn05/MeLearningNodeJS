c8632e34daee620a8bf7ae929bb928426a09ce08
ajohn <ajohn@suyati.com>
2023-08-07 15:53:08 +0530

    premitive and reference types

--- MyFirstCode.js
+++ MyFirstCode.js
@@ -1,2 +1,70 @@
 const fileSystem= require('fs');         //imports  the built in files system functionality to a varible for accessing it.
+const _=require('lodash');              // imports lodash as _
 fileSystem.writeFileSync('MyFile.txt','This is the first line i wrote to a file from node.js code.');  // params=>param1:'filename', param2:'content'
+
+// /************************* Reference Type example ***************************/
+// var person ={
+//     name:"Ajosh",
+//     Age:23,
+//     hobbies:['Cricket','Audio books']
+// };
+// console.log("\n","Person : " ,person,"\n");
+
+// var secondPerson=person;
+
+
+// person.name='John';
+
+// console.log("Second Person : " ,secondPerson,"\n");
+
+// /************************* Object.assign example ***************************/
+
+// var person ={
+//     name:"Ajosh",
+//     Age:23,
+//     hobbies:['Cricket','Audio books']
+// };
+// console.log("\n","Person : " ,person,"\n");
+
+// var secondPerson=Object.assign({},person);
+
+
+// person.name='John';
+
+// console.log("Second Person : " ,secondPerson,"\n");
+
+// /************************* Object.assign example: Not a deep copy ***************************/
+
+// var person ={
+//     name:"Ajosh",
+//     Age:23,
+//     hobbies:['Cricket','Audio books']
+// };
+// console.log("\n","Person : " ,person,"\n");
+
+// var secondPerson=Object.assign({},person);
+
+
+// person.name='John';
+
+// person.hobbies.push('Reading');
+
+// console.log("Second Person : " ,secondPerson,"\n");
+
+/************************* deep copy using slice() for arrays ***************************/
+
+var person ={
+    name:"Ajosh",
+    Age:23,
+    hobbies:['Cricket','Audio books']
+};
+console.log("\n","Person : " ,person,"\n");
+
+var secondPerson= _.cloneDeepWith(person);
+
+
+person.name='John';
+
+person.hobbies.push('Reading');
+
+console.log("Second Person : " ,secondPerson,"\n");
