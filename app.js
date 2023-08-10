04a57495f6bacf8630663a48ccfd0dfd13b2811b
ajohn <ajohn@suyati.com>
2023-08-10 15:14:50 +0530

    route intro

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //this file is the root of application
 
 const http= require('http');
+const fs=require("fs");
 const { constrainedMemory } = require('process');
 const { text } = require('stream/consumers');
 const server= http.createServer((request,response)=>{
@@ -30,21 +31,31 @@ const server= http.createServer((request,response)=>{
             postBody.push(chunk);
         });
         console.log(postBody);
-        const parsedBody=[];
-        request.on('end',()=>{
-            parsedBody.push(Buffer.concat(postBody).toString());
+        //const parsedBody=[];
+        const getParsedBody = () => {
+            const promise = new Promise((resolve, reject) => {
+                request.on('end',()=>{
+                    resolve((Buffer.concat(postBody).toString()).split('=')[1]);
+                });
+            });
+            return promise;
+          };
+          getParsedBody().then(parsedBody=>{
             response.write('<html>');
             response.write('<head><title>NodeServer</title></head>')
             response.write('<body>');
             response.write('<form action = "/received" method="POST">');
-            response.write('<h1> Message received '+ parsedBody[0] +'</h1>');
+            response.write('<h1> Message received : '+ parsedBody +'</h1>');
             response.write('<button type ="submit">OK</button>')
             response.write('</form>')
             response.write('</body>');
             response.write('</html>');
-            
-            return response.end();
-        });
+            return parsedBody;
+          }).then(parsedBody=>{
+            fs.writeFile('serverFile.txt',parsedBody,()=>{
+                return response.end();
+            });
+          });
         
     }
     if(request.url==="/received" && request.method==='POST')
